0abf9eee9f6b1c5888d6eff275f8ec3fdac3db47
faraz <farazul.islam@gmail.com>
2023-11-06 00:18:13 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const booking = document.getElementById('booking')
 const menu = document.getElementById('menu')
 const about = document.getElementById('about')
 const social = document.getElementById('social')
+const parties = document.getElementById('parties')
 
 function navToggle() {
     hamburger.classList.toggle('open');
@@ -12,6 +13,7 @@ function navToggle() {
     menu.classList.toggle('hidden')
     about.classList.toggle('hidden')
     social.classList.toggle('hidden')
+    parties.classList.toggle('hidden')
 }
 
 hamburger.addEventListener('click', navToggle);
